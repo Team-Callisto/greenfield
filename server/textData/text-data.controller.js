7af6c6c8f4d6bbf3c142562984f74d14002e9e8a
Eric Zayas <ezayas16@icloud.com>
2017-05-06 18:41:53 -0400

    saving to db

--- server/textData/text-data.controller.js
+++ server/textData/text-data.controller.js
@@ -1,40 +1,82 @@
-var request = require('request');
+const request = require('request');
+const bodyparser = require('body-parser');
+const textMsg = require('./text-data.model.js');
+const Q = require('q');
+const _ = require('underscore');
 
-var accountSid = 'ACa801ed88b159c5a2765254041e70c7f1';
-var authToken = 'a2c016d277659b7bb9283232324bd35f';
+const createLog = Q.nbind(textMsg.create, textMsg);
 
-var client = require('twilio')(accountSid, authToken);
+const accountSid = 'ACa801ed88b159c5a2765254041e70c7f1';
+const authToken = 'a2c016d277659b7bb9283232324bd35f';
 
+const client = require('twilio')(accountSid, authToken);
 
+const allMessages = {};
 module.exports = {
-  sendText : function(request,response,next) {
-    console.log('at server file listening for post requests to /text');
-    console.log(request);
-    var options = client.messages.create({
-            to: request.body.phoneNumber,
-            from: "+16319047332",
-            body: request.body.message,
-        }, function(err, message) {
-            console.log(message.sid);
-        });
+
+  sendText:
+    function(req, res, next) {
+    //             console.log('at server file listening for post requests to /text');
+    //             console.log(req.body.phoneNumber);
+    //                     let options = client.messages.create({
+    //                         uri: "https://api.twilio.com/2010-04-01/Accounts/ACa801ed88b159c5a2765254041e70c7f1/Messages.json",
+    //                         to: "+" + req.body.phoneNumber,
+    //                         from: "+16319047332",
+    //                         body: req.body.message,
+    //                     }, function(err, message) {
+    //                         if(err) return console.log("error here " + err)
+    //                         // console.log(message.sid);
+    //                     });
+    //
+    //
+    //                     request(options, function(err, res, body) {
+    //                         if (err) return console.log(err);
+    //                         console.log("this is body in request"+ body);
+    //                         res.send(body);
+    //                     });
+    //                     console.log("this is res: " + res);
+    //                     res.send(req.body);
+    const options = {
+      method: 'GET',
+      url: 'https://api.twilio.com/2010-04-01/Accounts/ACa801ed88b159c5a2765254041e70c7f1/Messages.json',
+      headers: {
+        'postman-token': '9542652f-2abd-87fd-3ffd-831e1f9b4708',
+        'cache-control': 'no-cache',
+        authorization: 'Basic QUNhODAxZWQ4OGIxNTljNWEyNzY1MjU0MDQxZTcwYzdmMTphMmMwMTZkMjc3NjU5YjdiYjkyODMyMzIzMjRiZDM1Zg==',
+        from: '+16319047332',
+        to: '+16318355557',
+        body: 'hello phone' }
+      };
+
+      request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+
+        console.log(body);
+      })
 
 
-    request(options,function(err,res,body){
-      if(err) return console.log(err);
-      res.send(body);
-    });
-  },
+    },
 
-  recieveText: function(request,response,next) {
-    console.log('at server file listening for posts requests to /text');
-    // console.log(request);
-    app.post('/recieve', function(req, res) {
-    var twiml = new twilio.TwimlResponse();
-    twiml.message(function() {
-      this.body('How are you feeling?');
-    });
-    res.writeHead(200, {'Content-Type': 'text/xml'});
-    res.end(twiml.toString());
-    });
-  }
-}
+
+  recieveText: function(req,res){
+      let msgFrom = req.body.From;
+      let msgBody = req.body.Body;
+      console.log(msgBody);
+      // res.send(
+      //     `<Response>
+      //       <Message>${msgFrom}. How are you feeling? Send Back Number between: -3 <-> 3</Message>
+      //     </Response>`
+      // );
+
+      return createLog({
+        phoneNumber: msgFrom,
+        message: msgBody
+      })
+      .fail(function (error) {
+        next(error);
+      });
+
+    }
+
+    // sendToFront:
+    }
