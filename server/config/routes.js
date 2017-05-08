a4f3131c1100e5126729ce90d540030c2d907742
Guangyu Xu <davidxu198556@gmail.com>
2017-05-08 17:30:37 -0400

    Clean server side code

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,12 +1,8 @@
-var logDataModel = require('../logData/log-data.model.js');//check this path!!!!!
-var logDataController = require('../logData/log-data.controller.js');//check this path!!!!!
+var logDataModel = require('../logData/log-data.model.js');
+var logDataController = require('../logData/log-data.controller.js');
 var userDataController = require('../userData/user-data.controller.js');
-
 var textController = require('../textData/text-data.controller.js');
 var helpers = require('./helpers.js');
-
-var helpers = require('./helpers.js');
-
 
 module.exports = function (app, express) {
   app.post('/db', logDataController.newLog);
@@ -17,13 +13,10 @@ module.exports = function (app, express) {
   app.post('/data/:year/:month/:day', logDataController.daily);
   app.post('/data/:year/:month', logDataController.monthly);
   app.post('/text', textController.sendText);
-
-
   app.post('/recieve',textController.recieveText);
   app.post('/message',textController.recieveText);
   app.post('/data/average/level/daily/:year', logDataController.dailyAverages);
   app.post('/data/average/level/weekly/:year', logDataController.weeklyAverages);
-
   app.use(helpers.errorLogger);
   app.use(helpers.errorHandler);
 }
--- server/textData/text-data.controller.js
+++ server/textData/text-data.controller.js
@@ -1,11 +1,8 @@
 const request = require('request');
 const bodyparser = require('body-parser');
-
 const accountSid = 'ACa801ed88b159c5a2765254041e70c7f1';
 const authToken = 'a2c016d277659b7bb9283232324bd35f';
-
 const client = require('twilio')(accountSid, authToken);
-
 
 module.exports = {
   allMessages : {
@@ -13,38 +10,32 @@ module.exports = {
   },
   sendText:
     function(req, res, next) {
-                console.log('at server file listening for post requests to /text');
-                console.log(req.body.phoneNumber);
-                        let options = client.messages.create({
-                            uri: "https://api.twilio.com/2010-04-01/Accounts/ACa801ed88b159c5a2765254041e70c7f1/Messages.json",
-                            to: "+" + req.body.phoneNumber,
-                            from: "+16319047332",
-                            body: req.body.message,
-                        }, function(err, message) {
-                            if(err) return console.log("error here " + err)
-                            // console.log(message.sid);
-                        });
-
-
-                        request(options, function(err, res, body) {
-                            if (err) return console.log(err);
-                            console.log(body);
-                            res.send(body);
-                        });
-                        console.log(res);
-                        res.send(req.body);
-                    },
-
+      console.log('at server file listening for post requests to /text');
+      console.log(req.body.phoneNumber);
+        let options = client.messages.create({
+          uri: "https://api.twilio.com/2010-04-01/Accounts/ACa801ed88b159c5a2765254041e70c7f1/Messages.json",
+          to: "+" + req.body.phoneNumber,
+          from: "+16319047332",
+          body: req.body.message,
+        }, function(err, message) {
+             if(err) return console.log("error here " + err);
+        });
+        request(options, function(err, res, body) {
+          if (err) return console.log(err);
+          console.log(body);
+          res.send(body);
+        });
+        console.log(res);
+        res.send(req.body);
+    },
 
   recieveText: function(req,res){
-      let msgFrom = req.body.From;
-      let msgBody = req.body.Body;
-      exports.allMessages.msgFrom = msgBody;
-      res.send(
-          `<Response>
-            <Message>${msgFrom}. How are you feeling </Message>
-          </Response>`
-      )}
-
-
-    }
+    let msgFrom = req.body.From;
+    let msgBody = req.body.Body;
+    exports.allMessages.msgFrom = msgBody;
+    res.send(
+      `<Response>
+        <Message>${msgFrom}. How are you feeling </Message>
+      </Response>`
+    )}
+  }
